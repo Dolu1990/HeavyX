ed53324019383731025a94d4a082d0cd9849055c
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-03-27 16:42:07 +0800

    add LLVM and Clang

--- derivations.nix
+++ derivations.nix
@@ -1,9 +1,12 @@
 { pkgs }:
-rec {
+let
+  llvm-src = pkgs.callPackage ./fetch-llvm-clang.nix {};
+in rec {
   yosys = pkgs.callPackage ./yosys.nix {};
   symbiyosys = pkgs.symbiyosys.override { inherit yosys; };
   nmigen = pkgs.callPackage ./nmigen.nix { inherit yosys; };
   jtagtap = pkgs.callPackage ./jtagtap.nix { inherit nmigen; };
   minerva = pkgs.callPackage ./minerva.nix { inherit nmigen; inherit jtagtap; };
   heavycomps = pkgs.callPackage ./heavycomps.nix { inherit nmigen; };
+  llvm-hx = pkgs.callPackage ./llvm-hx.nix { inherit llvm-src; };
 }
