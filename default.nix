e56d2ad3c877d805b56962a089c490a2f5b3d8ff
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-04 23:43:46 +0800

    style

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {}}:
 (import ./derivations.nix { inherit pkgs; }) // {
-	vivado = import ./eda/vivado.nix { inherit pkgs; };
+  vivado = import ./eda/vivado.nix { inherit pkgs; };
 }
