0ef16ba90c00d1c89472f29362d7c9b504a695e7
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-06 21:25:34 +0800

    llvm: use more up-to-date upstream, build for riscv

--- compilers/llvm-hx.nix
+++ compilers/llvm-hx.nix
@@ -1,28 +1,24 @@
 { stdenv
-, fetchFromGitHub, runCommand
+, fetchurl, runCommand
 , perl, groff, cmake, libxml2, python, libffi, valgrind
 , ...
 }:
 
 let
-  llvm-src = fetchFromGitHub {
-    rev = "527aa86b578da5dfb9cf4510b71f0f46a11249f7";
-    owner = "m-labs";
-    repo = "llvm-or1k";
-    sha256 = "0lmcg9xj66pf4mb6racipw67vm8kwm84dl861hyqnywd61kvhrwa";
+  llvm-src = fetchurl {
+    url = "https://releases.llvm.org/7.0.1/llvm-7.0.1.src.tar.xz";
+    sha256 = "16s196wqzdw4pmri15hadzqgdi926zln3an2viwyq0kini6zr3d3";
   };
-  clang-src = fetchFromGitHub {
-    rev = "9e996136d52ed506ed8f57ef8b13b0f0f735e6a3";
-    owner = "m-labs";
-    repo = "clang-or1k";
-    sha256 = "0w5f450i76y162aswi2c7jip8x3arzljaxhbqp8qfdffm0rdbjp4";
+  clang-src = fetchurl {
+    url = "https://releases.llvm.org/7.0.1/cfe-7.0.1.src.tar.xz";
+    sha256 = "067lwggnbg0w1dfrps790r5l6k8n5zwhlsw7zb6zvmfpwpfn4nx4";
   };
   combined-src = runCommand "llvm-clang-src" {}
     ''
     mkdir -p $out
     mkdir -p $out/tools/clang
-    cp -r ${llvm-src}/* $out/
-    cp -r ${clang-src}/* $out/tools/clang
+    tar xf ${llvm-src} -C $out --strip-components=1
+    tar xf ${clang-src} -C $out/tools/clang --strip-components=1
     '';
 in
   stdenv.mkDerivation rec {
@@ -37,12 +33,12 @@ in
       mkdir -p $out/
     '';
 
-    cmakeFlags = with stdenv; [
+    cmakeFlags = [
       "-DCMAKE_BUILD_TYPE=Release"
       "-DLLVM_BUILD_LLVM_DYLIB=ON"
       "-DLLVM_LINK_LLVM_DYLIB=ON"
       "-DLLVM_TARGETS_TO_BUILD=X86"
-      "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=RISCV;OR1K"
+      "-DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD=RISCV"
       "-DLLVM_ENABLE_ASSERTIONS=OFF"
       "-DLLVM_INSTALL_UTILS=ON"
       "-DLLVM_INCLUDE_TESTS=OFF"
