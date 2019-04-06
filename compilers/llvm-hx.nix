298514fe0aa1421776515029ebcd86c0cd2b233b
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-06 15:20:49 +0800

    move fetch-llvm-clang.nix into llvm-hx.nix

--- compilers/llvm-hx.nix
+++ compilers/llvm-hx.nix
@@ -1,16 +1,33 @@
 { stdenv
-, git
-, callPackage
+, fetchFromGitHub, runCommand
 , perl, groff, cmake, libxml2, python, libffi, valgrind
 , ...
 }:
 
 let
-  llvm-src = callPackage ./fetch-llvm-clang.nix {};
+  llvm-src = fetchFromGitHub {
+    rev = "527aa86b578da5dfb9cf4510b71f0f46a11249f7";
+    owner = "m-labs";
+    repo = "llvm-or1k";
+    sha256 = "0lmcg9xj66pf4mb6racipw67vm8kwm84dl861hyqnywd61kvhrwa";
+  };
+  clang-src = fetchFromGitHub {
+    rev = "9e996136d52ed506ed8f57ef8b13b0f0f735e6a3";
+    owner = "m-labs";
+    repo = "clang-or1k";
+    sha256 = "0w5f450i76y162aswi2c7jip8x3arzljaxhbqp8qfdffm0rdbjp4";
+  };
+  combined-src = runCommand "llvm-clang-src" {}
+    ''
+    mkdir -p $out
+    mkdir -p $out/tools/clang
+    cp -r ${llvm-src}/* $out/
+    cp -r ${clang-src}/* $out/tools/clang
+    '';
 in
   stdenv.mkDerivation rec {
     name = "llvm-hx";
-    src = llvm-src;
+    src = combined-src;
 
     buildInputs = [ perl groff cmake libxml2 python libffi ] ++ stdenv.lib.optional stdenv.isLinux valgrind;
 
