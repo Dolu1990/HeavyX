2bd819fcbe633ac341ba4dc3ff2996cbdf939d01
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-18 11:58:51 +0800

    roundrobin: use nmigen zero-width signals

--- heavycomps/heavycomps/roundrobin.py
+++ heavycomps/heavycomps/roundrobin.py
@@ -9,16 +9,12 @@ class RoundRobin:
 
     def elaborate(self, platform):
         m = Module()
-        n = self.n
-        if n > 1:
-            with m.Switch(self.grant):
-                for i in range(n):
-                    with m.Case(i):
-                        with m.If(~self.request[i]):
-                            for j in reversed(range(i+1, i+n)):
-                                t = j % n
-                                with m.If(self.request[t]):
-                                    m.d.sync += self.grant.eq(t)
-        else:
-            m.d.comb += self.grant.eq(0)
+        with m.Switch(self.grant):
+            for i in range(self.n):
+                with m.Case(i):
+                    with m.If(~self.request[i]):
+                        for j in reversed(range(i+1, i+self.n)):
+                            t = j % self.n
+                            with m.If(self.request[t]):
+                                m.d.sync += self.grant.eq(t)
         return m
