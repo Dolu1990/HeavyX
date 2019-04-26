263b04245ef9107dee58fccfa403ee7862fe033c
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-04-26 18:27:06 +0800

    add continuous build for helloworld examples

--- release.nix
+++ release.nix
@@ -1,5 +1,9 @@
 { pkgs ? import <nixpkgs> {}}:
 let
   derivations = import ./derivations.nix { inherit pkgs; };
+  jobs = derivations // {
+    helloworld_ecp5 = import ./examples/helloworld_ecp5.nix { inherit pkgs; };
+    helloworld_kintex7 = import ./examples/helloworld_kintex7.nix { inherit pkgs; };
+  };
 in
-  builtins.mapAttrs (name: value: pkgs.lib.hydraJob value) derivations
+  builtins.mapAttrs (name: value: pkgs.lib.hydraJob value) jobs
